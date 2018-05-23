45d6365c13d537cf0d7e6c08f900549b5f13aa58
Jed Murdock <jed@plyinteractive.com>
2018-05-23 11:14:15 -0700

    add initial Post model

---
+++ database/migrations/2018_05_23_172615_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->dateTimeTz('visible_at');
+            $table->string('title',100);
+            $table->string('slug',100);
+            $table->mediumText('body');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
